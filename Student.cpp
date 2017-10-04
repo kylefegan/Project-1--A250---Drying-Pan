ba04d461dbad48ce79027d0fa883dc74faf4f009
kylefegan <32467192+kylefegan@users.noreply.github.com>
2017-10-04 16:51:59 -0700

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,5 +1,9 @@
 #include "Student.h"
 
+#include <iostream>
+#include <string>
+#include <vector>
+
 // default constructor
 
 
@@ -28,10 +32,42 @@
 
 
 // printStudentInfo
-
+void Student::printStudentInfo() const
+{
+  cout << getID() << " -- ";
+  printName();
+}
 
 // printStudentInfo (overloaded)
-
+void Student::printStudentInfo(double tuition) const
+{
+  //Name
+  cout << "Student Name: ";
+  printName();
+  cout << endl;
+  
+  //ID & Enrolled Courses
+  cout << "Student ID: " << getID() << endl;
+  cout << "Number of courses enrolled: " << getNumberOfCourses() << endl << endl;
+  
+  //Course List
+  cout << "Course No  Course Name  Credits Grade" << endl;
+  //No need for setw as the header is constant
+  coursesEnrolled.printCourseInfo();
+  cout << endl;
+  
+  //Credit Hours & GPA
+  cout << "Total number of credit hours: " << getCreditsEnrolled() << endl;
+  if (isTuitionPaid) cout << "Current Term GPA: " << getGpa() << endl;
+  else
+  {
+    cout << "*** Grades are being held for not paying the tuition. ***" << endl;
+    cout << "Amount Due: $" << tuition << endl;
+  }
+  
+  //Footer
+  cout << endl << "-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-" << endl << endl;
+}
 
 // getCoursesEnrolled
 
