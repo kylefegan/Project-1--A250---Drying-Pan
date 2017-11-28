c6395f8516b7c9c8c5bd2ede378b10c502a1a9bb
mngo43 <32467216+mngo43@users.noreply.github.com>
2017-11-27 16:38:57 -0800

    Add files via upload

---
+++ project_1_FULL/Project1/StudentListChild.cpp
@@ -0,0 +1,26 @@
+/************************
+FlavorTown
+
+Ngo, Matthew
+Fegan, Kyle
+Manning, Tristan
+Cortes, Andre
+
+CS A250 - Fall 2017
+
+Project 2
+*************************/
+
+#include "StudentListChild.h"
+
+// default constructor
+StudentListChild::StudentListChild() {}
+
+//printStudentsInOrder
+void StudentListChild::printStudentsInOrder() const
+{
+	//Andre
+}
+
+// destructor
+StudentListChild::~StudentListChild() {}
---
+++ project_1_FULL/Project1/StudentListChild.h
@@ -0,0 +1,40 @@
+/************************
+FlavorTown
+
+Ngo, Matthew
+Fegan, Kyle
+Manning, Tristan
+Cortes, Andre
+
+CS A250 - Fall 2017
+
+Project 2
+*************************/
+
+#ifndef STUDENTLISTCHILD_H
+#define STUDENTLISTCHILD_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "StudentList.h"
+
+using namespace std;
+
+class StudentListChild : public StudentList
+{
+public:
+
+	// default constructor
+	StudentListChild();
+
+	//printStudentsInOrder
+	void printStudentsInOrder() const;
+
+	// destructor
+	~StudentListChild();
+};
+
+
+#endif
