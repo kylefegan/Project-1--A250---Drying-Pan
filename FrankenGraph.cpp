c0b1436d9def994bea0e0ab51d9cecbf20a0306b
acortes417 <acortes417@gmail.com>
2018-03-05 22:58:28 -0800

    Update FrankenGraph.cpp

--- FrankenGraph.cpp
+++ FrankenGraph.cpp
@@ -37,8 +37,130 @@ void FrankenGraph::createGraph(const vector<vector<string>>& blueprint)
 		count++;
 	}
 }
+//copy constructor
+Graph::Graph(const Graph& otherGraph)
+{
+	capacity = otherGraph.capacity;
+	verts = otherGraph.verts;
 
-int main()
+	int count = 0;
+	while (otherGraph.ptrToSucc[count])
+	{
+		ptrToVerts[count] = otherGraph.ptrToVerts[count];
+		ptrToSucc[count] = otherGraph.ptrToSucc[count];
+		count++;
+	}
+}
+//move constructor
+Graph::Graph(Graph&& otherGraph)
+{
+	verts = otherGraph.verts;
+	capacity = otherGraph.capacity;
+	ptrToSucc = otherGraph.ptrToSucc;
+	ptrToVerts = otherGraph.ptrToVerts;
+
+	otherGraph.verts = 0;
+	otherGraph.capacity = 0;
+	delete[] otherGraph.ptrToSucc;
+	otherGraph.ptrToSucc = nullptr;
+	delete[] otherGraph.ptrToVerts;
+	otherGraph.ptrToVerts = nullptr;
+}
+//assignment operator
+Graph& Graph::operator=(const Graph& otherGraph)
+{
+	if (&otherGraph != this)
+	{
+		//if the array we are passing has a different
+		//  capacity from the calling object,
+		//  then we need to create a new array
+		if (capacity != otherGraph.capacity)
+		{
+			//deallocate the memory used by 
+			//  the calling object and
+			//  re-create the object so that 
+			//  it has the same capacity
+			delete[] ptrToSucc;
+			ptrToSucc = new AnyList*[otherGraph.capacity];
+			delete[] ptrToVerts;
+			ptrToVerts = new string[otherGraph.capacity];
+			//update capacity
+			capacity = otherGraph.capacity;
+			verts = otherGraph.verts;
+		}
+
+		//update number of elements	
+		capacity = otherGraph.capacity;
+
+		// start copying
+		int count = 0;
+		while(otherGraph.ptrToSucc[count])
+		{
+			ptrToVerts[count] = otherGraph.ptrToVerts[count];
+			ptrToSucc[count] = otherGraph.ptrToSucc[count];
+			count++;
+		}
+	}
+	else
+	{
+		cerr << "Attempted assignment to itself.";
+	}
+
+	return *this;
+}
+//move assignment operator
+Graph& Graph::operator=(Graph&& otherGraph)
+{	
+	if (this != &otherGraph)
+	{
+		verts = otherGraph.verts;
+		capacity = otherGraph.capacity;
+		otherGraph.capacity = 0;
+		otherGraph.verts = 0;
+		//if we are just moving pointers we will have to delete the other array
+		delete[] ptrToVerts;
+		ptrToVerts = otherGraph.ptrToVerts;
+		otherGraph.ptrToVerts = nullptr;
+
+		delete[] ptrToSucc;
+		ptrToSucc = otherGraph.ptrToSucc;
+		otherGraph.ptrToSucc = nullptr;
+	}
+	else
+		cerr << "Attempted assignment of itself." << endl;
+
+	return *this;
+}
+//destructroy graph
+void Graph::destroyGraph()
+{
+	//since the destructor would call destroyGraph
+	//if we call destroy graph before this and dont properly 
+	//delete the object the destructor will call this function causing an error
+	if (capacity > 0)
+	{
+		int count = 0;
+		while (ptrToSucc[count])
+		{
+			ptrToSucc[count]->destroyList();
+			count++;
+		}
+		delete[] ptrToSucc;
+		ptrToSucc = nullptr;
+		delete[] ptrToVerts;
+		ptrToVerts = nullptr;
+		capacity = 0;
+		verts = 0;
+	}
+	else
+		cerr << "List is already empty" << endl;
+}
+//destructor
+Graph::~Graph()
+{
+	destroyGraph();
+}
+int main(
 {
 	vector<vector<string>> data =
 	{
